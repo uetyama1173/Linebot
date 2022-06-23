663b80aa61acfb6f1700eeb3091bce896d2cf7c3
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-06-23 16:38:43 +0900

    conten

--- index.js
+++ index.js
@@ -26,92 +26,97 @@ app.post("/webhook", function (req, res) {
             messages: [
 
                 {
-                    "type": "bubble",
-                    "hero": {
-                        "type": "image",
-                        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-                        "size": "full",
-                        "aspectRatio": "20:13",
-                        "aspectMode": "cover",
-                        "action": {
-                            "type": "uri",
-                            "uri": "http://linecorp.com/"
+                    "type": "flex",
+                    "altText": "This is a Flex Message",
+                    "contents": {
+
+
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover",
+                            "action": {
+                                "type": "uri",
+                                "uri": "http://linecorp.com/"
+                            }
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "年代を教えて下さい",
+                                    "weight": "bold",
+                                    "size": "xl",
+                                    "margin": "none",
+                                    "align": "center"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "baseline",
+                                    "margin": "md",
+                                    "contents": []
+                                }
+                            ],
+                            "borderColor": "#696969",
+                            "cornerRadius": "30px"
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "style": "link",
+                                    "height": "sm",
+                                    "action": {
+                                        "type": "postback",
+                                        "label": "10~20代",
+                                        "data": "low"
+                                    }
+                                },
+                                {
+                                    "type": "button",
+                                    "style": "link",
+                                    "height": "sm",
+                                    "action": {
+                                        "type": "postback",
+                                        "label": "30~40代",
+                                        "data": "middle"
+                                    }
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [],
+                                    "margin": "sm"
+                                },
+                                {
+                                    "type": "button",
+                                    "action": {
+                                        "type": "postback",
+                                        "label": "50~60代",
+                                        "data": "high"
+                                    }
+                                },
+                                {
+                                    "type": "button",
+                                    "action": {
+                                        "type": "postback",
+                                        "label": "60代以上",
+                                        "data": "age60"
+                                    }
+                                }
+                            ],
+                            "flex": 0
                         }
-                    },
-                    "body": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "年代を教えて下さい",
-                                "weight": "bold",
-                                "size": "xl",
-                                "margin": "none",
-                                "align": "center"
-                            },
-                            {
-                                "type": "box",
-                                "layout": "baseline",
-                                "margin": "md",
-                                "contents": []
-                            }
-                        ],
-                        "borderColor": "#696969",
-                        "cornerRadius": "30px"
-                    },
-                    "footer": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "spacing": "sm",
-                        "contents": [
-                            {
-                                "type": "button",
-                                "style": "link",
-                                "height": "sm",
-                                "action": {
-                                    "type": "postback",
-                                    "label": "10~20代",
-                                    "data": "low"
-                                }
-                            },
-                            {
-                                "type": "button",
-                                "style": "link",
-                                "height": "sm",
-                                "action": {
-                                    "type": "postback",
-                                    "label": "30~40代",
-                                    "data": "middle"
-                                }
-                            },
-                            {
-                                "type": "box",
-                                "layout": "vertical",
-                                "contents": [],
-                                "margin": "sm"
-                            },
-                            {
-                                "type": "button",
-                                "action": {
-                                    "type": "postback",
-                                    "label": "50~60代",
-                                    "data": "high"
-                                }
-                            },
-                            {
-                                "type": "button",
-                                "action": {
-                                    "type": "postback",
-                                    "label": "60代以上",
-                                    "data": "age60"
-                                }
-                            }
-                        ],
-                        "flex": 0
                     }
-                }
-            ]
+                }]
         })
 
         // リクエストヘッダー
