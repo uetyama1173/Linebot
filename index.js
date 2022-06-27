fa324ee8ae074cfec74765fdba70a78e765168e0
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-06-28 08:19:48 +0900

    heroku

--- index.js
+++ index.js
@@ -41,9 +41,11 @@ app.post("/webhook", function (req, res) {
     res.send("HTTP POST request sent to the webhook URL!")
     // ユーザーがボットにメッセージを送った場合、返信メッセージを送る
     console.log(req.body.events[0].type === "message")
+    console.log(postback.data)
     // 文字列化したメッセージデータ
     const dataString = JSON.stringify({
         replyToken: req.body.events[0].replyToken,
+
         messages: [
             {
 
@@ -143,6 +145,10 @@ app.post("/webhook", function (req, res) {
                 }
 
             }
+
+
+
+
 
         ]
     })
