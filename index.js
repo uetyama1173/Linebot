4e1d7f86ff3ec3e1aaf4e6379df6b781c9b49b17
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-07-02 21:01:40 +0900

    aaa

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ app.post("/webhook", function (req, res) {
 
     //postback-data age 格納
 
+   
+const agedayo =  console.log(req.body.events[0].postback.data)
     console.log(Object.values(agepostback))
     const data1 = Object.values(agepostback)
 
