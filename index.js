49dbeaaf6cd541b10254163fc4d21a34110c62df
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-07-03 11:27:43 +0900

    sinario

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ app.get("/", (req, res) => {
 
 
 app.post("/webhook", function (req, res) {
-    res.send("HTTP POST request sent to the webhook URL!")
+   // res.send("HTTP POST request sent to the webhook URL!")
     //ユーザーがボットにメッセージを送った場合、返信メッセージを送る
-    //↓ このコマンドでJSONdataを出力している
-    //console.log(req.body.events[0].message.text == 'あああ')
+    //↓ このコマンドは 特定の文字列 がどうかを判定している 
+    const judge = console.log(req.body.events[0].message.text == 'あああ')
+
     console.log(req.body.events[0])
 
 
