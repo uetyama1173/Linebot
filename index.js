3163a705c3ef99645186d5d77673fc460915a216
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-07-03 09:29:22 +0900

    sinario

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/webhook", function (req, res) {
 
 
     //postback-data age 格納
-    if (str.match(/postback/)) {
+    if (req.body.events[0].match(/postback/)) {
 
         const agedata = (req.body.events[0].postback.data.scenario)
 
