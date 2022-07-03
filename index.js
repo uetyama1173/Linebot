38a029c2b0cf43054a271b245ba1db0aa025873b
*bipb4CPgwE <show_0310k@yahoo.co.jp>
2022-07-03 11:17:02 +0900

    sinario

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.post("/webhook", function (req, res) {
     const dataString2 = JSON.stringify({
         replyToken: req.body.events[0].replyToken,
 
-        messages[
+        messages: [
             {
 
                 "type": "flex",
